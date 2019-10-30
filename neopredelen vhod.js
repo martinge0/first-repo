58e66f0f91fb915fe45c6ce14c86d6295d2766a2
Martin Georgiev <pchelpbs@gmail.com>
2019-10-30 09:41:06 +0200

    Update neopredelen vhod.js

--- neopredelen vhod.js
+++ neopredelen vhod.js
@@ -1,4 +1,5 @@
-function uniteUnique(arr) {
+function uniteUnique() {
     let inputArr = Array.prototype.slice.call(arguments);
-    console.log(inputArr)
+    console.log(inputArr)// exit is [ [ 1, 3, 2 ], [ 5, 2, 1, 4 ], [ 2, 1 ] ]
   }
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
